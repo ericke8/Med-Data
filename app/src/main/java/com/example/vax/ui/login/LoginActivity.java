1e817d0e8e66b841e8cac1f7f57022a9219be6a3
yimingnzhao <90603yahoo@gmail.com>
2019-04-27 00:57:20 -0700

    Merge branch 'master' of https://github.com/ericke8/Vax
    # Conflicts:
    #	.idea/vcs.xml
    #	app/src/main/java/com/example/vax/ui/login/LoginActivity.java

--- app/src/main/java/com/example/vax/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/vax/ui/login/LoginActivity.java
@@ -19,6 +19,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.vax.R;
+import com.example.vax.data.MainActivity;
 import com.example.vax.data.ScrollingActivity;
 import com.example.vax.ui.login.LoginViewModel;
 import com.example.vax.ui.login.LoginViewModelFactory;
@@ -29,6 +30,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import java.net.URL;
+import 	javax.net.ssl.HttpsURLConnection;
+import android.os.AsyncTask;
+
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
@@ -44,8 +50,6 @@ public class LoginActivity extends AppCompatActivity {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                Button button = (Button)findViewById(R.id.button2);
-                button.setText("Click Me !");
                 // Create URL
                 URL githubEndpoint = null;
                 try {
