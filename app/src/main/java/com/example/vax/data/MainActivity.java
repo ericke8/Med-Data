04c69a75a19c47e1fdcc3f0299e4d45b46a8efce
yimingnzhao <90603yahoo@gmail.com>
2019-04-27 02:37:00 -0700

    added api call to meds

--- app/src/main/java/com/example/vax/data/MainActivity.java
+++ app/src/main/java/com/example/vax/data/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.text.method.ScrollingMovementMethod;
+import android.util.JsonReader;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,7 +18,11 @@ import com.example.vax.R;
 
 import com.example.vax.ui.login.LoginActivity;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -66,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                 // Create URL
                 URL githubEndpoint = null;
                 try {
-                    githubEndpoint = new URL("https://api.github.com/");
+                    githubEndpoint = new URL("https://api.fda.gov/drug/enforcement.json?search=report_date:[20040101+TO+20131231]&limit=100");
                 } catch (Exception e) {
 
                 }
@@ -76,8 +83,19 @@ public class MainActivity extends AppCompatActivity {
                     if (myConnection.getResponseCode() == 200) {
                         // Success
                         // Further processing here
+                        InputStream responseBody = myConnection.getInputStream();
+                        InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
+                        JsonReader jsonReader = new JsonReader(responseBodyReader);
+
+                        // Do something with the value
+
+                        TextView textView = findViewById(R.id.json);
+                        textView.setMovementMethod(new ScrollingMovementMethod());
+                        textView.setText(getDrugs(jsonReader));
+
                     } else {
-                        // Error handling code goes here
+                        TextView textView = findViewById(R.id.json);
+                        textView.setText("Error Connecting to Database...");
                     }
                 } catch (Exception e) {
 
@@ -94,6 +112,50 @@ public class MainActivity extends AppCompatActivity {
         String message = text.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public String getDrugs(JsonReader jsonReader){
+        String ans = "";
+        try {
+            jsonReader.beginObject(); // Start processing the JSON object
+            while (jsonReader.hasNext()) {
+                String key = jsonReader.nextName();
+                if (key.equals("results")) {
+                    jsonReader.beginArray();
+                    while (jsonReader.hasNext()) {
+                        ans += getDrugName(jsonReader);
+                        ans += "\n";
+                    }
+                    jsonReader.endArray();
+                } else {
+                    jsonReader.skipValue();
+                }
+            }
+            jsonReader.close();
+            return ans;
+        } catch (Exception e) {
+            return "DRUGS";
+        }
+    }
+
+    public String getDrugName(JsonReader jsonReader) {
+        try {
+            String keyName = "product_description";
+            String ans = null;
+            jsonReader.beginObject();
+            while (jsonReader.hasNext()) {
+                String name = jsonReader.nextName();
+                if (name.equals(keyName)) {
+                    ans = jsonReader.nextString();
+                } else {
+                    jsonReader.skipValue();
+                }
+            }
+            jsonReader.endObject();
+            return ans;
+        } catch (Exception e) {
+            return "HEROIN";
+        }
     }
 
 }
