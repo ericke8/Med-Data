3c2384e06a8197fa372ea458a7d397be3a909d1a
yimingnzhao <90603yahoo@gmail.com>
2019-04-27 17:32:33 -0700

    can search for medicines

--- app/src/main/java/com/example/vax/data/MainActivity.java
+++ app/src/main/java/com/example/vax/data/MainActivity.java
@@ -277,7 +277,7 @@ public class MainActivity extends AppCompatActivity {
             ans = tempDate + ": " + tempName;
             return ans;
         } catch (Exception e) {
-            return "HEROIN";
+            return "FOOD";
         }
     }
 
