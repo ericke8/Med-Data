a891cbc4ce106e546c953acc313f2eee24506c06
xannem <41248956+xannem@users.noreply.github.com>
2019-04-27 17:48:31 -0700

    Recall Info

---
+++ app/src/main/java/com/example/vax/data/InfoRecall.java
@@ -0,0 +1,21 @@
+package com.example.vax.data;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.vax.R;
+
+public class InfoRecall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_add_meds);
+
+        final EditText addMedText = findViewById(R.id.addMedsText);
+        final Button addButton = findViewById(R.id.addMedsButton);
+    }
+}
